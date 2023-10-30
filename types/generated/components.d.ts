c4182ebe4684c5e5a2c92dd00cdca28accff54c4
akash <email@example.com>
2023-10-30 21:59:20 +0530

    first commiet

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,23 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ProductListingProductListing extends Schema.Component {
+  collectionName: 'components_product_listing_product_listings';
+  info: {
+    displayName: 'Product Listing';
+    icon: 'picture';
+  };
+  attributes: {
+    name: Attribute.String;
+    image: Attribute.Media;
+    stock: Attribute.String;
+    color: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'product-listing.product-listing': ProductListingProductListing;
+    }
+  }
 }
